d48ce9b061812ddc84fff43bbe0c5b17a23bcb6f
rickygh12 <81440077+rickygh12@users.noreply.github.com>
2021-04-17 09:59:53 -0500

    rg3

--- github21/ViewController.swift
+++ github21/ViewController.swift
@@ -1,19 +1,12 @@
-//
 //  ViewController.swift
 //  github21
-//
 //  Created by rvcstudent on 4/17/21.
 // Ricardo Gordiano
-// i like the tech-bus 
+// i like the tech-bus
 import UIKit
-
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
