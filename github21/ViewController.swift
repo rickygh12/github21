ed0033ede22c01bfa151167c4e8991c30658fa65
rickygh12 <81440077+rickygh12@users.noreply.github.com>
2021-04-17 10:00:46 -0500

    rg4

--- github21/ViewController.swift
+++ github21/ViewController.swift
@@ -3,6 +3,7 @@
 //  Created by rvcstudent on 4/17/21.
 // Ricardo Gordiano
 // i like the tech-bus
+// Today is april 17 2021
 import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
